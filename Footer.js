89d5f94c9221b004a797fd9dba72ff714c6d985b
Sarath Nakka <101663778+sarathnakka@users.noreply.github.com>
2024-03-30 18:58:38 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="socialMedia">
+        <InstagramIcon /> <TwitterIcon /> <FacebookIcon /> <LinkedInIcon />
+      </div>
+      <p> &copy; 2024 Sarath Nakka</p>
+    </div>
+  );
+}
+
+export default Footer;
